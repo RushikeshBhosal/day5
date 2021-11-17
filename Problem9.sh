d0a4edadd58022848b5334d3f8f874547f8f5f4a
RushikeshBhosal <rushikeshbhosale001@gmai.com>
2021-11-17 03:17:23 -0800

    case

---
+++ Problem9.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+read -p "Enter the Number : " a
+
+case $a in
+        "0")
+                echo "Zero";;
+        "1")
+                echo "One";;
+        "2")
+                echo "Two";;
+        "3")
+                echo "Three";;
+        "4")
+                echo "Four";;
+        "5")
+                echo "Five";;
+        "6")
+                echo "Six";;
+        "7")
+                echo "Seven";;
+        "8")
+                echo "Eight";;
+        "9")
+                echo "Nine";;
+
+        *)
+                echo "value out off range..| ";;
+esac
