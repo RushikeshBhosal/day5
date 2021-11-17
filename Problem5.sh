7aaac8114c16af49159ffd48f704ecfdd4697ee0
RushikeshBhosal <rushikeshbhosale001@gmai.com>
2021-11-17 03:09:53 -0800

    if elif single digit

---
+++ Problem5.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+
+read -p "Enter a number between 1 to 9 inclusive > " character
+if [ "$character" = "1" ];
+then
+     echo " one "
+elif [ "$character" = "2" ];
+then
+    echo " two "
+elif [ "$character" =  "3" ];
+then
+     echo " three "
+elif [ "$character" =  "4" ];
+then
+      echo "four"
+elif [ "$character" =  "5" ];
+then
+     echo " five "
+elif [ "$character" =  "6" ];
+then
+     echo " six "
+
+elif [ "$character" =  "7" ];
+then
+     echo " seven "
+elif [ "$character" =  "8" ];
+then
+     echo " eight "
+elif [ "$character" =  "9" ];
+then
+     echo " nine "
+else
+     echo" You did not enter a number between 1 and 9. "
+fi
